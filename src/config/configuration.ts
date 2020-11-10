4aae19d3fe8aa6a89acd9ab01d53a981ee5040ea
Sergio <asgarth@gmail.com>
2020-11-10 18:01:44 +0100

    Rename config variables to avoid naming conflicts

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,15 +1,15 @@
 export default () => ({
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PEAKD_PLUGIN_PORT, 10) || 3000,
   database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    database: process.env.DATABASE_NAME || 'hive',
-    username: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PWD,
+    host: process.env.PEAKD_PLUGIN_DB_HOST || 'localhost',
+    port: parseInt(process.env.PEAKD_PLUGIN_DB_PORT, 10) || 5432,
+    database: process.env.PEAKD_PLUGIN_DB_NAME || 'hive',
+    username: process.env.PEAKD_PLUGIN_DB_USER,
+    password: process.env.PEAKD_PLUGIN_DB_PWD,
     pool: {
-      minConnections: parseInt(process.env.DATABASE_POOL_MIN, 10) || 5,
-      maxConnections: parseInt(process.env.DATABASE_POOL_MAX, 10) || 20,
-      idle: parseInt(process.env.DATABASE_POOL_IDLE, 10) || 30000,
+      minConnections: parseInt(process.env.PEAKD_PLUGIN_DB_POOL_MIN, 10) || 5,
+      maxConnections: parseInt(process.env.PEAKD_PLUGIN_DB_POOL_MAX, 10) || 20,
+      idle: parseInt(process.env.PEAKD_PLUGIN_DB_POOL_IDLE, 10) || 30000,
     },
   }
 });
