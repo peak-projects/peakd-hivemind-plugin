cc94b6bf7b798e83ec5a88dd1e324f552b5be046
Sergio <asgarth@gmail.com>
2020-11-21 21:58:31 +0100

    Fix CSP policy for OpenAPI

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import compression from 'fastify-compress';
 import helmet from 'fastify-helmet';
 import * as fs from 'fs';
-import { config } from 'process';
 import { AppModule } from './app.module';
+
+const packageJson = require('../package.json');
+const version = packageJson?.version || 'dev';
 
 declare const module: any;
 
@@ -28,26 +30,35 @@ async function bootstrap() {
     new FastifyAdapter({ ...httpsOptions, logger: true, maxParamLength: 2000 })
   );
 
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+
   const options = new DocumentBuilder()
     .setTitle('PeakD Hivemind Plugin')
     .setDescription('A Node.js appplication to add additional API to Hive/Hivemind nodes')
-    .setVersion('0.1')
+    .setVersion(version)
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   app.register(compression);
-  app.register(helmet);
+  app.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
   app.enableCors();
 
   const configService = app.get(ConfigService);
   const port = configService.get('port') || 3000;
 
   await app.listen(port, '0.0.0.0');
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
